69606539c84dd77caba0d331f3624e0394e61d39
AndreiSanto <50970072+AndreiSanto@users.noreply.github.com>
2021-08-31 08:36:14 -0300

    Subindo classes

---
+++ classes/classe.php
@@ -0,0 +1,25 @@
+<?php
+class Usuario
+{
+    private $idUsuario;
+    private $nome;
+    private $email;
+    private $login;
+    private $senha;
+    function Usuario()
+    {
+    }
+
+
+}
+class Assunto{
+private $idAssunto;
+private $titulo;
+
+}
+class Mensagem{
+private $idAssunto;
+private $idMensagem;
+
+
+}
