1801c6fa0dad1f247b12c68a4ded98bc5c710d3a
AndreiSanto <50970072+AndreiSanto@users.noreply.github.com>
2021-09-01 22:04:29 -0300

    add dao usuario

---
+++ dao/UsuarioDao.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once('../conexao/conexao.php');
+require_once('../classes/classe.php');
+class UsuarioDao
+{
+    private $con;
+
+    public function UsuarioDao()
+    {
+        $conexao = new Conexao();
+        $this->con = $conexao->getConexao();
+    }
+    public function inserirUsuario(Usuario $usuario)
+    {
+        $sql = $this->con->prepare("insert into usuario (nome,email,login,senha) values(:nom,:em,:log,:se)");
+        $sql->bindValue(':nom', $usuario->getNome());
+        $sql->bindValue(':em', $usuario->getEmail());
+        $sql->bindValue(':log', $usuario->getLogin());
+        $sql->bindValue(':se', $usuario->getSenha());
+        $sql->execute();
+    }
+}
